edb9a81653c70ab88c346f92f6ab25fbf1936228
Alena <ABelova0605@gmail.com>
2023-03-27 00:04:08 +0300

    add changes into exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -60,8 +60,24 @@ def check_chosen_note(notes_list, index, msg):
             return notes_list
         elif action.isalpha() and action.lower() == 'no':
             print()
-            return f"You refused to {msg} the note.\n"
+            return f"You refused to {msg} the note."
         else:
             print('=' * 50)
             print("Incorrect input! Please, enter 'yes' or 'no'.")
+            print('=' * 50)
+
+
+def check_day():
+    while True:
+        try:
+            number = int(input(f"Enter the date of month: "))
+            if 1 <= number <= 31:
+                return number
+            else:
+                print('=' * 50)
+                print("Incorrect input! The number must be in the range from 1 to 31!")
+                print('=' * 50)
+        except ValueError:
+            print('=' * 50)
+            print("Incorrect input! It's not a number!")
             print('=' * 50)
