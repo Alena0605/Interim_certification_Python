c595a89ccfe134862815ae29f7b2749d65f838bd
Alena <ABelova0605@gmail.com>
2023-03-27 00:03:26 +0300

    add changes into views

--- views/view.py
+++ views/view.py
@@ -1,6 +1,7 @@
 from controller.note_controller import NoteController
 import views.user_interface as u_i
 import exceptions as ex
+import log
 
 
 class View:
@@ -15,62 +16,14 @@ class View:
 
             if op == 1:
                 print("You've chosen to add note.\n")
-                print(self.__n_c.create_note())
+                res = self.__n_c.create_note()
+                log.logg("1. Add note", res)
+                print(res)
+                print()
 
             elif op == 2:
                 print("You've chosen to search note.\n")
-
-                while True:
-                    u_i.search_note()
-                    ad_op1 = ex.check_input("Enter the number from the list")
-
-                    if ad_op1 == 1:
-                        print("You've chosen to search note by ID.\n")
-                        print(self.__n_c.search_note_by_id())
-
-                    elif ad_op1 == 2:
-                        print("You've chosen to search note by TITLE.\n")
-                        print(self.__n_c.search_note_by_title())
-
-                    elif ad_op1 == 3:
-                        print("You've chosen to search note by DATE.\n")
-
-                        while True:
-                            u_i.search_by_date()
-                            ad_op2 = ex.check_input("Enter the number from the list")
-
-                            if ad_op2 == 1:
-                                print("You've chosen to search note by Month.\n")
-
-                                while True:
-                                    u_i.search_by_month()
-                                    month = ex.check_input("Enter the month number")
-
-                                    if 1 <= month <= 12:
-                                        print(self.__n_c.search_note_by_month(month))
-                                        break
-                                    elif month == 0:
-                                        print()
-                                        break
-                                    else:
-                                        u_i.incorrect_input()
-
-                            elif ad_op2 == 2:
-                                print("Sorry! This function is not available yet! :(\n")
-
-                            elif ad_op2 == 0:
-                                print()
-                                break
-
-                            else:
-                                u_i.incorrect_input()
-
-                    elif ad_op1 == 4:
-                        print()
-                        break
-
-                    else:
-                        u_i.incorrect_input()
+                self.__menu_search_note()
 
             elif op == 3:
                 print("You've chosen to change or delete note.\n")
@@ -81,11 +34,17 @@ class View:
 
                     if ad_op == 1:
                         print("You've chosen to change note by ID.\n")
-                        print(self.__n_c.change_note())
+                        res = self.__n_c.change_note()
+                        log.logg("3. Change / delete note -> 1. Change note by ID", res)
+                        print(res)
+                        print()
 
                     elif ad_op == 2:
                         print("You've chosen to delete note by ID.\n")
-                        print(self.__n_c.delete_note())
+                        res = self.__n_c.delete_note()
+                        log.logg("3. Change / delete note -> 2. Delete note by ID", res)
+                        print(res)
+                        print()
 
                     elif ad_op == 3:
                         print()
@@ -97,6 +56,7 @@ class View:
             elif op == 4:
                 print("You've chosen to show all notes.\n")
                 print(self.__n_c.read_all())
+                print()
 
             elif op == 5:
                 print("Sorry! This function is not available yet! :(\n")
@@ -105,5 +65,68 @@ class View:
                 print("The program has finished working.")
                 break
 
+            else:
+                u_i.incorrect_input()
+
+    def __menu_search_note(self):
+        while True:
+            u_i.search_note()
+            ad_op1 = ex.check_input("Enter the number from the list")
+
+            if ad_op1 == 1:
+                print("You've chosen to search note by ID.\n")
+                print(self.__n_c.search_note_by_id())
+
+            elif ad_op1 == 2:
+                print("You've chosen to search note by TITLE.\n")
+                print(self.__n_c.search_note_by_title())
+
+            elif ad_op1 == 3:
+                print("You've chosen to search note by DATE.\n")
+                self.__menu_search_by_date()
+
+            elif ad_op1 == 4:
+                print()
+                break
+
+            else:
+                u_i.incorrect_input()
+
+    def __menu_search_by_date(self):
+        while True:
+            u_i.search_by_date()
+            ad_op2 = ex.check_input("Enter the number from the list")
+
+            if ad_op2 == 1:
+                print("You've chosen to search note by Month.\n")
+                self.__menu_search_by_month_day(ad_op2)
+
+            elif ad_op2 == 2:
+                print("You've chosen to search note by Month and Day.\n")
+                self.__menu_search_by_month_day(ad_op2)
+
+            elif ad_op2 == 0:
+                print()
+                break
+
+            else:
+                u_i.incorrect_input()
+
+    def __menu_search_by_month_day(self, operation: int):
+        while True:
+            u_i.search_by_month()
+            month = ex.check_input("Enter the month number")
+
+            if 1 <= month <= 12:
+                if operation == 1:
+                    print(self.__n_c.search_note_by_date(month))
+                    break
+                elif operation == 2:
+                    day = ex.check_day()
+                    print(self.__n_c.search_note_by_date(month, day))
+                    break
+            elif month == 0:
+                print()
+                break
             else:
                 u_i.incorrect_input()
